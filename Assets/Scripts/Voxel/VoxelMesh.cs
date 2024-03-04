d204ad2132b5663bc9c3241cdc29282e4d2b01bd
Zackaria <scehr@outlook.com>
2024-03-04 21:27:11 +0000

    Voxel testing.

---
+++ Assets/Scripts/Voxel/VoxelMesh.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Essence.Voxel
+{
+    public class VoxelMesh : MonoBehaviour
+    {
+        public List<Vector3Int> gridPoints = new List<Vector3Int>();
+        public float halfSize = 0.1f;
+        public Vector3 localOrigin;
+
+        public SkinnedMeshRenderer skinnedMesh;
+
+        private void Start()
+        {
+            skinnedMesh = GetComponent<SkinnedMeshRenderer>();
+            VoxelizeMesh();
+        }
+
+        public Vector3 PointToWorldPosition(Vector3Int point)
+        {
+            float size = halfSize * 2f;
+            Vector3 position =
+                new Vector3
+                (
+                    halfSize + point.x * size,
+                    halfSize + point.y * size,
+                    halfSize + point.z * size
+                );
+
+            return localOrigin + transform.TransformPoint(position);
+        }
+
+        public void VoxelizeMesh()
+        {
+            Bounds bounds = skinnedMesh.bounds;
+            Vector3 minExtents = bounds.center - bounds.extents;
+            Vector3 count = bounds.extents / halfSize;
+
+            int xMax = Mathf.CeilToInt(count.x);
+            int yMax = Mathf.CeilToInt(count.y);
+            int zMax = Mathf.CeilToInt(count.z);
+
+            gridPoints.Clear();
+            localOrigin = transform.InverseTransformPoint(minExtents);
+
+            for (int x = 0; x < xMax; ++x)
+            {
+                for (int z = 0; z < zMax; ++z)
+                {
+                    for (int y = 0; y < yMax; ++y)
+                    {
+                        Vector3 pos = PointToWorldPosition(new Vector3Int(x, y, z));
+                        if (Physics.CheckBox(pos, new Vector3(halfSize, halfSize, halfSize)))
+                        {
+                            gridPoints.Add(new Vector3Int(x, y, z));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
