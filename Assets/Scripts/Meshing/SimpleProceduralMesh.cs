0b27011bf0ce67902d12a7bb8aaf6b90ca7ae9ca
Zackaria <scehr@outlook.com>
2024-04-02 21:31:54 +0100

    Learning proc meshing...

--- Assets/Scripts/Meshing/SimpleProceduralMesh.cs
+++ Assets/Scripts/Meshing/SimpleProceduralMesh.cs
@@ -9,8 +9,36 @@ namespace Essence
 
         private void OnEnable()
         {
-            Mesh mesh = new Mesh {
+            Mesh mesh = new Mesh
+            {
                 name = "Procedural Mesh"
+            };
+
+            mesh.vertices = new Vector3[]
+            {
+                Vector3.zero, Vector3.right, Vector3.up
+            };
+
+            mesh.normals = new Vector3[]
+            {
+                Vector3.back, Vector3.back, Vector3.back
+            };
+
+            mesh.tangents = new Vector4[]
+            {
+                new Vector4(1f, 0f, 0f, -1f),
+                new Vector4(1f, 0f, 0f, -1f),
+                new Vector4(1f, 0f, 0f, -1f)
+            };
+
+            mesh.uv = new Vector2[]
+            {
+                Vector2.zero, Vector2.right, Vector2.up
+            };
+
+            mesh.triangles = new int[]
+            {
+                0, 2, 1
             };
 
             GetComponent<MeshFilter>().mesh = mesh;
